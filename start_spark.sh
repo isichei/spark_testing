85e7491473ce86905cf738e5a4b7f9d60347a3a6
Karik Isichei <karik.isichei@gmail.com>
2019-03-21 14:10:00 +0000

    Exposing port for spark UI

--- start_spark.sh
+++ start_spark.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -it -v /Users/karik:/home/jovyan --rm -p 9999:8888 jupyter/all-spark-notebook start.sh jupyter lab
+docker run -it -v /Users/karik:/home/jovyan --rm -p 9999:8888 -p 9988:4040 jupyter/all-spark-notebook start.sh jupyter lab
